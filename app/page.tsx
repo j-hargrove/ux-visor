a48a9d5cda5e6372f4524a7cda63ded443b59c58
j-hargrove <jonathan@jonathanhargrove.com>
2026-04-29 14:02:47 -0600

    fix: remove Object.entries

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,6 @@ export default function Home() {
 
   const steps = Object.keys(dataset);
 
-  const entries = Object.entries(dataset) as [string, StepData][];
 
   type StepData = {
   users: number;
